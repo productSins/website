cbe571cb74d2b866d7619a56c9dd03e67e6150e0
unknown <jyzhou@umass.edu>
2020-08-23 12:04:01 -0400

    key update

--- backend/productSins/settings.py
+++ backend/productSins/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lar7w9bmmpa70%%^!_3f@#dsfuaf9cbdeeey6^^dzz+503*&y3'
+SECRET_KEY = os.environ.get('productSinsDjangoKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
